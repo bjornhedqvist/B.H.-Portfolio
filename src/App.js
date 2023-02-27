b6f8f70b22be830ef228db99e01b2cebcfd99a6f
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-02-26 19:13:30 -0800

    title changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App h-100 cover-container d-flex w-100  p-3 mx-auto flex-column">
       <header className="mb-auto">
         <div>
-          <h3 className="float-md-start">Bjorn Hedqvist</h3>
+          <h3 className="float-md-start">Björn Hedqvist</h3>
           <nav className="nav nav-masthead justify-content-center float-md-end">
             <a
               className="nav-link fw-bold py-1 px-0 active"
