f4ceb61db1a4e4eff0779ab0b7572704ece20c33
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-03-14 13:48:56 -0700

    temporary visual hold
    until i can continue development on the other tabs of the navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,7 @@ function App() {
             >
               Home
             </a>
-            <a className="nav-link fw-bold py-1 px-0" href="#">
+            {/* <a className="nav-link fw-bold py-1 px-0" href="#">
               Projects
             </a>
             <a className="nav-link fw-bold py-1 px-0" href="#">
@@ -25,7 +25,7 @@ function App() {
             </a>
             <a className="nav-link fw-bold py-1 px-0" href="#">
               About Me
-            </a>
+            </a> */}
           </nav>
         </div>
       </header>
